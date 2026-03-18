9ccc7cae22896ce09a9342982e66b1b08c0d2f9b
ShadowDev <teammicro2026@outlook.com>
2026-03-18 11:43:11 +0100

    Update moduleapps.js

--- Content/js/moduleapps.js
+++ Content/js/moduleapps.js
@@ -25,3 +25,47 @@ document.addEventListener("click", (e) => {
     closeWindow(id);
   }
 });
+function makeDraggable(windowEl) {
+  const header = windowEl.querySelector(".titlebar");
+  if (!header) return;
+
+  let offsetX = 0;
+  let offsetY = 0;
+  let isDragging = false;
+
+  function startDrag(e) {
+    isDragging = true;
+
+    const rect = windowEl.getBoundingClientRect();
+
+    const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+    const clientY = e.touches ? e.touches[0].clientY : e.clientY;
+
+    offsetX = clientX - rect.left;
+    offsetY = clientY - rect.top;
+  }
+
+  function drag(e) {
+    if (!isDragging) return;
+
+    const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+    const clientY = e.touches ? e.touches[0].clientY : e.clientY;
+
+    windowEl.style.left = (clientX - offsetX) + "px";
+    windowEl.style.top = (clientY - offsetY) + "px";
+  }
+
+  function stopDrag() {
+    isDragging = false;
+  }
+
+  // Mouse
+  header.addEventListener("mousedown", startDrag);
+  document.addEventListener("mousemove", drag);
+  document.addEventListener("mouseup", stopDrag);
+
+  // Touch
+  header.addEventListener("touchstart", startDrag);
+  document.addEventListener("touchmove", drag);
+  document.addEventListener("touchend", stopDrag);
+}
